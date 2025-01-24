5bfec7f13aac2a27293a8103c7cfe6a4a0128425
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-24 16:52:31 +0530

    feat: added, provided and consumed ReviewContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import Nav from './components/Nav'
+import ReviewContext from './contexts/ReviewContext'
 
 function Reporting() {
-
+const {initialItems}=useContext(ReviewContext)
 
   return (
     <>
-      <Nav/>
+      <Nav  />
       <hr/>
       <main className='container'>
-      <h1>status</h1>
+      <h1>status ({initialItems.length})</h1>
+      <hr/>
+      <h2>Pending:</h2>
+      <p className='fs-2'>{initialItems.filter(item=>!item.isReviewed).length}</p>
+      <hr/>
+      <h2>Reviewed:</h2>
+      <p className='fs-2'>{initialItems.filter(item=>item.isReviewed).length}</p>
       </main>
       
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,15 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 import Pending from './pages/Pending.jsx'
 import Reporting from './App.jsx'
+import ReviewContext from './contexts/ReviewContext.jsx'
+const initialReviewItems=[{
+  url:"https://github.com/repo1",
+  isReviewd:false
+},
+{
+  url:"https://github.com/repo2",
+  isReviewd:false
+}]
 const router=createBrowserRouter([
   {path:"/",
   element:<Reporting/>
@@ -15,8 +24,10 @@ const router=createBrowserRouter([
     element:<Pending/>
   }
 ])
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(//wrapped whole app inside the ReviewContext
   <StrictMode>
+    <ReviewContext.Provider value={{initialItems:initialReviewItems}}>
    <RouterProvider router={router}/>
+   </ReviewContext.Provider>
   </StrictMode>,
 )
